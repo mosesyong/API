a9b9e450af042066ee1864bf02983466541fe1b5
moses <moses_yong@yahoo.com.sg>
2018-08-12 17:59:02 +0800

    Changed connection manager
    Added online linux server integration

--- src/java/dao/ConnectionManager.java
+++ src/java/dao/ConnectionManager.java
@@ -11,136 +11,124 @@ import java.util.logging.Logger;
  * A class that manages connections to the database. It also has a utility
  * method that close connections, statements and resultsets
  */
-/**
- *
- * @author Moses
- */
 public class ConnectionManager {
-    private static final String PROPS_FILENAME = "connection.properties";
-    private static String dbUser;
-    private static String dbPassword;
-    private static String dbURL;
 
-    static {
-        readDatabaseProperties();
+  private static final String PROPS_FILENAME = "connection.properties";
+  private static String dbUser;
+  private static String dbPassword;
+  private static String dbURL;
 
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, null, ex);
+  static {
+    readDatabaseProperties();
+
+    initDBDriver();
+  }
+
+  private static void readDatabaseProperties() {
+    InputStream is = null;
+    try {
+      // Retrieve properties from connection.properties via the CLASSPATH
+      // WEB-INF/classes is on the CLASSPATH
+      is = ConnectionManager.class.getResourceAsStream(PROPS_FILENAME);
+      Properties props = new Properties();
+      props.load(is);
+
+      // load database connection details
+      String host = props.getProperty("db.host"); //get from connection properties file
+      String port = props.getProperty("db.port"); //get from connection properties file
+      String dbName = props.getProperty("db.name"); //get from connection properties file
+      dbUser = props.getProperty("db.user");
+      
+      // grab environment variable to check if we are on production environment
+        String osName = System.getProperty("os.name");
+        if (osName.equals("Linux")) {
+            // in production environment, use aws.db.password
+            dbPassword = props.getProperty("aws.db.password");
+        } else {
+            // in local environment, use db.password
+            dbPassword = props.getProperty("db.password");
         }
+
+
+      dbURL = "jdbc:mysql://" + host + ":" + port + "/" + dbName;
+    } catch (Exception ex) {
+      // unable to load properties file
+      String message = "Unable to load '" + PROPS_FILENAME + "'.";
+      Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, message, ex);
+      throw new RuntimeException(message, ex);
+    } finally {
+      if (is != null) {
+        try {
+          is.close();
+        } catch (IOException ex) {
+          Logger.getLogger(ConnectionManager.class.getName()).log(Level.WARNING, "Unable to close connection.properties", ex);
+        }
+      }
     }
+  }
+  
+  private static void initDBDriver() {
+    try {
+      Class.forName("com.mysql.jdbc.Driver").newInstance();
+    } catch (Exception ex) {
+      // unable to load properties file
+      String message = "Unable to find JDBC driver for MySQL.";
 
-    /**
-     * to read database properties file 
-     */
-    private static void readDatabaseProperties() {
-        InputStream is = null;
-        try {
-            // Retrieve properties from connection.properties via the CLASSPATH
-            // WEB-INF/classes is on the CLASSPATH
-            is = ConnectionManager.class.getResourceAsStream(PROPS_FILENAME);
-            Properties props = new Properties();
-            props.load(is);
-
-            // load database connection details
-            String host = props.getProperty("db.host");
-            String port = props.getProperty("db.port");
-            String dbName = props.getProperty("db.name");
-            dbUser = props.getProperty("db.user");
-
-            // grab environment variable to check if we are on production environment
-            String osName = System.getProperty("os.name");
-            
-            if (osName.equals("Linux")) {
-                // in production environment, use aws.db.password
-                dbPassword = props.getProperty("aws.db.password");
-            } else {
-                // in local environment, use db.password
-                dbPassword = props.getProperty("db.password");
-            }
-            dbURL = "jdbc:mysql://" + host + ":" + port + "/" + dbName;
-        } catch (Exception ex) {
-            // unable to load properties file
-            String message = "Unable to load '" + PROPS_FILENAME + "'.";
-
-            System.out.println(message);
-            Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, message, ex);
-            throw new RuntimeException(message, ex);
-        } finally {
-            if (is != null) {
-                try {
-                    is.close();
-                } catch (IOException ex) {
-                    Logger.getLogger(ConnectionManager.class.getName()).log(Level.WARNING, "Unable to close connection.properties", ex);
-                }
-            }
-        }
+      System.out.println(message);
+      Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, message, ex);
+      throw new RuntimeException(message, ex);
     }
+  }
 
-    /**
-     * to initialize the database driver
-     */
-    private static void initDBDriver() {
-        try {
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-        } catch (Exception ex) {
-            // unable to load properties file
-            String message = "Unable to find JDBC driver for MySQL.";
+  /**
+   * Gets a connection to the database
+   *
+   * @return the connection
+   * @throws SQLException if an error occurs when connecting
+   */
+  public static Connection getConnection() throws SQLException {
+    String message = "dbURL: " + dbURL
+            + "  , dbUser: " + dbUser
+            + "  , dbPassword: " + dbPassword;
+    Logger.getLogger(ConnectionManager.class.getName()).log(Level.INFO, message);
 
-            System.out.println(message);
-            Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, message, ex);
-            throw new RuntimeException(message, ex);
-        }
+    return DriverManager.getConnection(dbURL, dbUser, dbPassword);
+
+  }
+
+
+
+  /**
+   * close the given connection, statement and resultset
+   *
+   * @param conn the connection object to be closed
+   * @param stmt the statement object to be closed
+   * @param rs the resultset object to be closed
+   */
+  public static void close(Connection conn, Statement stmt, ResultSet rs) {
+    try {
+      if (rs != null) {
+        rs.close();
+      }
+    } catch (SQLException ex) {
+      Logger.getLogger(ConnectionManager.class.getName()).log(Level.WARNING,
+              "Unable to close ResultSet", ex);
     }
-
-    /**
-     * Gets a connection to the database
-     *
-     * @return the connection
-     * @throws SQLException if an error occurs when connecting
-     */
-    public static Connection getConnection() throws SQLException {
-        String message = "dbURL: " + dbURL
-                + "  , dbUser: " + dbUser
-                + "  , dbPassword: " + dbPassword;
-        Logger.getLogger(ConnectionManager.class.getName()).log(Level.INFO, message);
-
-        return DriverManager.getConnection(dbURL, dbUser, dbPassword);
-
+    try {
+      if (stmt != null) {
+        stmt.close();
+      }
+    } catch (SQLException ex) {
+      Logger.getLogger(ConnectionManager.class.getName()).log(Level.WARNING,
+              "Unable to close Statement", ex);
     }
-
-    /**
-     * close the given connection, statement and resultset
-     *
-     * @param conn the connection object to be closed
-     * @param stmt the statement object to be closed
-     * @param rs the resultset object to be closed
-     */
-    public static void close(Connection conn, Statement stmt, ResultSet rs) {
-        try {
-            if (rs != null) {
-                rs.close();
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(ConnectionManager.class.getName()).log(Level.WARNING,
-                    "Unable to close ResultSet", ex);
-        }
-        try {
-            if (stmt != null) {
-                stmt.close();
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(ConnectionManager.class.getName()).log(Level.WARNING,
-                    "Unable to close Statement", ex);
-        }
-        try {
-            if (conn != null) {
-                conn.close();
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(ConnectionManager.class.getName()).log(Level.WARNING,
-                    "Unable to close Connection", ex);
-        }
+    try {
+      if (conn != null) {
+        conn.close();
+      }
+    } catch (SQLException ex) {
+      Logger.getLogger(ConnectionManager.class.getName()).log(Level.WARNING,
+              "Unable to close Connection", ex);
     }
+  }
 }
